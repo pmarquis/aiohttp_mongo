1557f313be57808b5380dc7bc3c8a6bb1c589f0c
Pierre Marquis <pier.marquis@gmail.com>
2022-12-20 23:21:59 +0100

    Added /add_person

--- server.py
+++ server.py
@@ -1,6 +1,10 @@
 import aiohttp
 from aiohttp import web
 import asyncio
+import motor.motor_asyncio
+
+client = motor.motor_asyncio.AsyncIOMotorClient()
+db = client["app_database"]
 
 async def handle(request):
     name = request.match_info.get('name', "Anonymous")
@@ -9,13 +13,17 @@ async def handle(request):
 
 async def add_person(request):
     name = request.match_info.get('name')
+    print("add_person " + name)
     if not name:
         raise web.HTTPBadRequest('No name given')
-    return web.Response(
+    res = await db["persons"].insert_one({'name': name})    
+    print(res)
+    return web.Response(text="Added " + name)
 
 app = web.Application()
 app.add_routes([web.get('/', handle),
-                web.get('/{name}', handle)])
+                web.get('/{name}', handle),
+                web.get('/add_person/{name}', add_person)])
 
 if __name__ == '__main__':
     web.run_app(app)
